ab866dad82ad8fc7c234b1b8fa91129aa703fd78
iTzFabregas <fabricio-sampaio@usp.br>
2023-04-04 12:43:53 -0300

    feat: starting structure

---
+++ lib/graph.hpp
@@ -0,0 +1,24 @@
+#ifndef __GRAPH_HPP__
+#define __GRAPH_HPP__
+
+#include <iostream>
+#include <map>
+#include <list>
+
+using namespace std;
+
+class Graph
+{
+private:
+    int num_vert;
+    map<int, list<int>> graph_map;
+
+public:
+    Graph(/* args */);
+
+    ~Graph();
+
+    void add_edge(int v1, int v2);
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+    // le o nome do arquivo de entrada
+    char filename[16];
+    cin >> filename;
+
+    
+}
